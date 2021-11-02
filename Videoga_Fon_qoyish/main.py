686ff47a473c0395c54e05585db16f35fa4d5190
Sun'iy Idrok <karateeeesuper@gmail.com>
2021-11-02 22:38:29 +0500

    Update main.py

--- Videoga_Fon_qoyish/main.py
+++ Videoga_Fon_qoyish/main.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 video=cv2.VideoCapture(r'green3.mp4')
 video2=cv2.VideoCapture(r'b.mp4')
 
+out = cv2.VideoWriter('natija1.avi',cv2.VideoWriter_fourcc(*'DIVX'), 30, (1920,1080))
+rasm_toplam = []
 if not video.isOpened():
     print("Video yakunlandi")
     
@@ -11,12 +13,15 @@ while video.isOpened() and video2.isOpened():
     ret,frame=video.read()
     ret1,frame1=video2.read()
     
+    
     if not (ret and ret1):
         break
     
+    frame = cv2.resize(frame,(1920,1080),interpolation=cv2.INTER_LINEAR)
+    frame1 = cv2.resize(frame1,(1920,1080),interpolation=cv2.INTER_LINEAR)
     frame_copy=np.copy(frame)
     
-    lower_green=np.array([0,160,0])
+    lower_green=np.array([0,150,0])
     upper_green=np.array([150,255,150])
     
     mask=cv2.inRange(frame,lower_green,upper_green)
@@ -26,11 +31,16 @@ while video.isOpened() and video2.isOpened():
     frame1[mask==0]=[0,0,0]
     
     result=frame1+frame_copy
+    rasm_toplam.append(result)
     cv2.imshow('Frame',result)
     
-    if cv2.waitKey(25) & 0xFF==ord('q'):
+    if cv2.waitKey(1) & 0xFF==ord('q'):
         break
+        
+for i in rasm_toplam:
+    out.write(i)
     
 video2.release()
 video.release()
+out.release()
 cv2.destroyAllWindows()
